369158097f39de70882334a41ceb2fea57538eb3
MazzMS <maximo.a.olivera@gmail.com>
2024-06-24 21:31:38 -0300

    refactoring of converter.py

--- src/converter.py
+++ src/converter.py
@@ -29,6 +29,16 @@ def text_node_to_html_node(text_node):
         case _:
             raise ValueError(f"{text_node.text_type.name} is not supported")
 
+def identify_delimiter(text, index):
+    if text[index:index+2] == "**":
+        return "**", 2, "bold"
+    elif text[index] == "*":
+        return text[index], 1, "italic"
+    elif text[index] == "`":
+        return text[index], 1, "code"
+    else:
+        return None, 1, None 
+
 def parse_text(node):
     
     if not isinstance(node, TextNode):
@@ -38,49 +48,49 @@ def parse_text(node):
     current_segment = ""
     stack = [] # Tuple with delimiter string and index
     text = node.text
+    is_nested = False
 
     i = 0
     while i < len(text):
-        
-        # print(f"Current stack: {stack}\nCurrent char: \"{text[i]}\"\nCurrent segment: \"{current_segment}\"\nCurrent nodes: {parsed_nodes}\n")
-        
-        if text[i] in ["*", "`"]:  # Check for delimiters 
-            if text[i:i+2] in ["**"]: # Check also the next char in case it is bold
-                delimiter = ( "**", i)
-                step = 2
-            else: # If it isn't bold then save the delimiter as the current char
-                delimiter = (text[i], i)
-                step = 1
-            
-            # print(f"\n| Text: {text}\n| I found a delimiter, which is: {delimiter} at {i} index")
+        delimiter, step, text_type = identify_delimiter(text, i)
 
-            if stack: # If stack is empy
-                if stack[-1][0] == delimiter[0]: # and the delimiter matches with the current delimiter
-                    # print("| It's a closing delimiter")
+        print(f"Current stack: {stack}\nCurrent char: \"{text[i]}\"\nCurrent segment: \"{current_segment}\"\nCurrent nodes: {parsed_nodes}\n")
+        
+        if delimiter:  # Check for delimiters 
+            print(f"\n| Text: '{text}'\n| I found a delimiter, which is: {delimiter} at {i} index\n")
+
+            if stack: # If stack isn't empty
+                if stack[-1][0] == delimiter: # and the delimiter matches with the current delimiter
+                    print("\n| It's a closing delimiter")
                     start_index = stack.pop()[1]
-                    nested_text = text[start_index + len(delimiter[0]):i]
-                    # Process any inner text recursively
-                    if delimiter[0] == "**":
-                        # print(f"\n | I would append this text: \"{nested_text}\" as \"bold\"\n")
-                        parsed_nodes.append(TextNode(nested_text, "bold"))
-                    elif delimiter[0] == "*":
-                        # print(f"\n | I would append this text: \"{nested_text}\" as \"italic\"\n")
-                        parsed_nodes.append(TextNode(nested_text, "italic"))
-                    elif delimiter[0] == "`":
-                        # print(f"\n | I would append this text: \"{nested_text}\" as \"code\"\n")
-                        parsed_nodes.append(TextNode(nested_text, "code"))
+                    nested_text = text[start_index + len(delimiter):i]
+                    
+                    if not text_type:
+                        raise Exception(f"A delimiter '{delimiter}' was found but no text_type was assigned")
+
+                    if is_nested:
+                        is_nested = False
+                        print(f"\n| This text: '{nested_text}' has a nested element!\n| The parse_text function will be called with this text: '{nested_text}' and this text_type: '{text_type}'\n| The TextNode will be as follows: '{repr(TextNode(nested_text, text_type))}'")
+                        nested_element_list = parse_text(TextNode(nested_text, text_type))
+                        parsed_nodes.append(nested_element_list)
+                    else:
+                        parsed_nodes.append(TextNode(nested_text, text_type))
+                        print(f"\n| I would append this text: \"{nested_text}\" as \"{text_type}\"\n")
+                    
                     current_segment = ""
-                    step = len(delimiter[0])
+            
+                else: # If stack isn't empty but delimiter didn't match
+                    print(f"\n| Found delimiter, but it's not: '{stack[-1][0]}'. Instead, is: '{delimiter}'\n")
+                    is_nested = True
             else:
-                # print("| It's an opening delimiter\n")
+                print("\n| It's an opening delimiter\n")
                 if current_segment:
                     parsed_nodes.append(TextNode(current_segment, node.text_type.name))
                     current_segment = ""
                 # Push delimiter to stack and move index
-                stack.append(delimiter)
+                stack.append((delimiter, i))
         else:
             current_segment += text[i]
-            step = 1
         i += step
     
     # Append remaining text as a text node if any
@@ -91,12 +101,7 @@ def parse_text(node):
 
 def split_nodes_delimiter(old_nodes, delimiter=None, text_type="text"):
     new_nodes = []
-    for node in old_nodes: #first splitter
-        new_nodes.extend(parse_text(node))        
-    for i in range(len(new_nodes)):
-        for char in new_nodes[i].text:
-            if char in ["*", "`"]: # If the text of one of the returned nodes has a delimiter
-                new_nodes[i] = parse_text(new_nodes[i]) # Replace said node with a new list
-                break # Next node
+    for node in old_nodes:
+        new_nodes.extend(parse_text(node))
 
     return new_nodes

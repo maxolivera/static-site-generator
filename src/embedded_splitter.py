a7ed31c46d165f0327fb35b5d9badc5c4cc7c554
MazzMS <maximo.a.olivera@gmail.com>
2024-06-24 23:36:38 -0300

    added embedded, image and link, extraction

---
+++ src/embedded_splitter.py
@@ -0,0 +1,100 @@
+from textnode import (TextNode, TextTypes)
+
+from extractor import (extract_markdown_links, extract_markdown_images)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    current_segment = ""
+
+    for node in old_nodes:
+        text = node.text
+        images = extract_markdown_images(text)
+        
+        if not images: # If no images in text, just append and continue to next node
+            new_nodes.append(node)
+            continue
+
+        j = 0
+        while j < len(text):
+            if not images: # All images have been extracted
+                new_nodes.append(text[j:len(text)], "text")
+                break
+
+            char = text[j]
+            
+            # The below 'if' checks:
+            # First: if the character is an '!', which is the first char of an embedded image
+            # Second: if the slice after the '![' coincide with the alt text of the first image (image[0][0])
+            # Third: if the slice after the '(' (which would be {4 [because of "![""]("] + len of link.text} positions to {len of image.url}) coincide with the url of the image (image[0][1])
+
+            if char == "!" and text[j+2:j+len(images[0][0])+2] == images[0][0] and text[j+len(images[0][0])+4:j+len(images[0][0])+4+len(images[0][1])] == images[0][1]:
+                if current_segment: # If there is anything before the image, append it
+                    new_nodes.append(TextNode(current_segment, "text"))
+                
+                current_image = images.pop(0)
+                
+                # Append the TextNode of this image
+
+                new_nodes.append(TextNode(current_image[0], "image", current_image[1]))
+                
+                len_url = len(current_image[1])
+                len_alt_text = len(current_image[0])
+
+                j = j \
+                    + len_url \
+                    + len_alt_text \
+                    + 5 # ![]() characters
+            else:
+                current_segment += char
+                j += 1
+    
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    current_segment = ""
+
+    for node in old_nodes:
+        text = node.text
+        links = extract_markdown_links(text)
+        
+        if not links: # If no links in text, just append and continue to next node
+            new_nodes.append(node)
+            continue
+
+        j = 0
+        while j < len(text):
+            if not links: # All links have been extracted
+                new_nodes.append(text[j:len(text)], "text")
+                break
+
+            char = text[j]
+            
+            # The below 'if' checks:
+            # First: if the character is an '[' which is the first char of an embedded link
+            # Second: if the slice after the '[' coincide with the text of the first link (links[0][0])
+            # Third: if the slice after the '(' (which would be {3 [because of "[" "]("] + len of link.text} positions to {len of link.url}) coincide with the url of the link (links[0][1])
+
+            if char == "[" and text[j+1:j+len(links[0][0])+1] == links[0][0] and text[j+len(links[0][0])+3:j+len(links[0][0])+3+len(links[0][1])] == liks[0][1]:
+                if current_segment: # If there is anything before the link, append it
+                    new_nodes.append(TextNode(current_segment, "text"))
+                
+                current_link = images.pop(0)
+                
+                # Append the TextNode of this link
+
+                new_nodes.append(TextNode(current_image[0], "link", current_image[1]))
+                
+                len_text = len(current_link[0])
+                len_url = len(current_link[1])
+
+                j = j \
+                    + len_text \
+                    + len_url \
+                    + 4 # []() characters
+            else: # If not add the char to the string
+                current_segment += char
+                j += 1
+    
+    return new_nodes
---
+++ src/test_embedded_splitter.py
@@ -0,0 +1,54 @@
+import unittest
+
+from embedded_splitter import (split_nodes_link, split_nodes_image)
+
+from textnode import (TextTypes, TextNode)
+
+class TestEmbeddedSplitter(unittest.TestCase):
+    def test_split_image(self):
+        print("\n--- Testing Embedded Splitter: image ---\n"
+        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)"
+        expected_output = [
+            TextNode("This is text with an ", "text"),
+            TextNode("image", "image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode(" and another ", "text"),
+            TextNode("second image", "image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png"),
+        ]
+
+        nodes = split_nodes_image([TextNode(text, "text")])
+
+        try:
+            self.assertEqual(
+                expected_output,
+                nodes
+            )
+            print("\n--- End of test: Everything OK ---\n")
+        except AssertionError as ae:
+            print("\n| Test failed!\n")
+            print(f"\n| Text: '{text}'\n\n| Expected output: '{expected_output}'\n| Actual output: '{nodes}'")
+        except Exception as e:
+            print(f"\n| Something went wrong!\n | Exception: {e}\n")
+
+    def test_split_link(self):
+        print("\n--- Testing Embedded Splitter: link ---\n"
+        text = "This is text with an [image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another [second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)"
+        expected_output = [
+            TextNode("This is text with an ", "text"),
+            TextNode("image", "link", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode(" and another ", "text"),
+            TextNode("second image", "link", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png"),
+        ]
+
+        nodes = split_nodes_link([TextNode(text, "text")])
+
+        try:
+            self.assertEqual(
+                expected_output,
+                nodes
+            )
+            print("\n--- End of test: Everything OK ---\n")
+        except AssertionError as ae:
+            print("\n| Test failed!\n")
+            print(f"\n| Text: '{text}'\n\n| Expected output: '{expected_output}'\n| Actual output: '{nodes}'")
+        except Exception as e:
+            print(f"\n| Something went wrong!\n | Exception: {e}\n")
